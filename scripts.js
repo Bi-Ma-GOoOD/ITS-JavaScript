7a8113a134ecda9e3eb483385060faf0be3eb42c
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2026-09-03 15:55:22 +0700

    Studying try-catch-finally

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log("This is an external script!");
+function openMyFile() {
+    console.log("Open my file.");
+}
+
+function writeMyFile(data) {
+    if (!data){
+        throw new Error("Reference type error.");
+    }
+    console.log(`Write "${data}" into the file.`);
+}
+
+function handleError(error){
+    console.error(`Error: ${error}`);
+}
+
+function closeMyFile(){
+    console.log("Close my file.");
+}
+
+try {
+    openMyFile();
+    let theData = "Akrapon Phlayyai";
+    writeMyFile(theData); // This may throw an error
+} catch (e) {
+    handleError(e); // If an error occurred, handle it
+} finally {
+    closeMyFile(); // Always close the resource
+}
