a8ff24320adfbe943ddf04201fb45bb3c83fc3dd
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2026-09-07 11:42:49 +0700

    Exercise05 - window and global scope

--- scripts.js
+++ scripts.js
@@ -1,17 +1,7 @@
-// เก็บ URL ปัจจุบันไว้ในตัวแปร currentUrl
-let currentUrl = location.href;
-console.log(currentUrl);
+var greeting = "Hello";
+let farewell = "Bye";
 
-// ถามชื่อผู้ใช้ ถ้าไม่กรอกอะไรเลย (null หรือ string ว่าง) ให้แสดง alert เตือน
-let userName = prompt("กรุณากรอกชื่อของคุณ:");
-
-if (userName === null || userName === "") {
-    alert("คุณยังไม่ได้กรอกชื่อ!");
-} else {
-    console.log(`Input: ${userName}`);
-}
-
-// หน่วงเวลา 3 วินาที แล้วค่อยแสดงข้อความ "หมดเวลา!"
-setTimeout(() => {
-    console.log("หมดเวลา!");
-}, 3000);
+console.log(window.greeting); // Hello
+console.log(window.farewell); // undefined
+console.log(typeof window.greeting); // string
+console.log(typeof window.farewell); // undefined
