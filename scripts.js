096fbf788cd419a246cfc9d159f88fd078f0d167
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2026-09-07 10:49:08 +0700

    Exercise03 - Debug the code

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-let a = confirm("ยืนยันการลบ?");     // user กดปุ่ม "Cancel" -> false
-let b = prompt("กรอกอายุ:");          // user กดปุ่ม "Cancel" โดยไม่พิมพ์อะไร -> null
-let c = prompt("กรอกชื่อ:");          // user พิมพ์ "Tom" แล้วกด OK -> String
-let d = alert("บันทึกสำเร็จ");        // user กดปุ่ม OK -> undefined
+let userConfirmed = confirm("ต้องการดำเนินการต่อหรือไม่?");
+console.log(userConfirmed);
+
+if (userConfirmed) {
+    // location.href = "thankyou.html";
+    location.assign('./thankyou.html');
+} else {
+    location.assign('https://youtu.be/XRVQKihyACM?si=ru7S8Dkg-EmdVNfS'); // อันนี้ผมทำเล่นๆ นะคับ
+}
